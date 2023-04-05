6b9608e1591f65bce4898c1ff1f68406ffc27e09
Stéphane Claver DIBY <75357402+Stefan-ci@users.noreply.github.com>
2023-04-05 22:47:00 +0000

    Changed project langugage: fr-ci --> en-uk

--- django_gallery/settings.py
+++ django_gallery/settings.py
@@ -110,7 +110,7 @@ AUTH_PASSWORD_VALIDATORS = [
 # Internationalization
 # https://docs.djangoproject.com/en/4.1/topics/i18n/
 
-LANGUAGE_CODE = 'fr-ci'
+LANGUAGE_CODE = 'en-uk'
 
 TIME_ZONE = 'UTC'
 
