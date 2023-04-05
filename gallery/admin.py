d4e67d2da6690a809c2d257e9b368e4203260713
Stéphane Claver DIBY <75357402+Stefan-ci@users.noreply.github.com>
2023-04-05 21:57:11 +0000

    Created "Gallery" & "Image" models

--- gallery/admin.py
+++ gallery/admin.py
@@ -1,3 +1,29 @@
 from django.contrib import admin
+from gallery.models import Gallery, Image
 
-# Register your models here.
+
+
+
+
+class ImageAdmin(admin.ModelAdmin):
+    list_display = ['gallery', 'is_open_source', 'date']
+    list_filter = ['is_open_source', 'date']
+    search_fields = ['gallery__user__username', 'gallery__user__first_name', 'gallery__user__last_name', 'date', 'is_open_source']
+    date_hierarchy = 'date'
+
+
+
+class GalleryAdmin(admin.ModelAdmin):
+    list_display = ['user', 'date']
+    list_filter = ['date']
+    search_fields = ['user__username', 'user__first_name', 'user__last_name', 'date']
+    date_hierarchy = 'date'
+
+
+
+
+
+
+
+admin.site.register(Image, ImageAdmin)
+admin.site.register(Gallery, GalleryAdmin)
--- gallery/models.py
+++ gallery/models.py
@@ -1,3 +1,43 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+
+
+
+class Gallery(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    date = models.DateTimeField(auto_now_add=True)
+    
+    class Meta:
+        verbose_name = "Gallery"
+        verbose_name_plural = "Galleries"
+        ordering = ['-date']
+    
+    
+    def __str__(self):
+        return f"Gallery of {self.user.username}"
+
+
+
+
+
+
+class Image(models.Model):
+    gallery = models.ForeignKey(Gallery, on_delete=models.CASCADE)
+    image = models.ImageField(upload_to='images/%Y/%m/')
+    is_open_source = models.BooleanField(default=False, help_text="Check if you want to share this picture with everyone")
+    date = models.DateTimeField(auto_now_add=True)
+    
+    class Meta:
+        verbose_name = "Picture"
+        verbose_name_plural = "Pictures"
+        ordering = ['-date', 'gallery']
+
+
+    def __str__(self):
+        return f"Picture of {self.gallery.user.username}"
+    
+
+
+
+
