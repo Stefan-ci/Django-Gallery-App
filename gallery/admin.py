beb8e15cedb0f4535ea63993b52ff98a85eeb877
Stéphane Claver DIBY <75357402+Stefan-ci@users.noreply.github.com>
2023-04-05 22:05:44 +0000

    Custom methods on Gallery model for QuerySet stuff

--- gallery/admin.py
+++ gallery/admin.py
@@ -7,7 +7,7 @@ from gallery.models import Gallery, Image
 
 class ImageAdmin(admin.ModelAdmin):
     list_display = ['gallery', 'is_open_source', 'date']
-    list_filter = ['is_open_source', 'date']
+    list_filter = ['is_open_source', 'is_deleted', 'date']
     search_fields = ['gallery__user__username', 'gallery__user__first_name', 'gallery__user__last_name', 'date', 'is_open_source']
     date_hierarchy = 'date'
 
@@ -15,7 +15,7 @@ class ImageAdmin(admin.ModelAdmin):
 
 class GalleryAdmin(admin.ModelAdmin):
     list_display = ['user', 'date']
-    list_filter = ['date']
+    list_filter = ['is_active', 'date']
     search_fields = ['user__username', 'user__first_name', 'user__last_name', 'date']
     date_hierarchy = 'date'
 
--- gallery/models.py
+++ gallery/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from django.urls import reverse
 from django.contrib.auth.models import User
 
 
@@ -6,6 +7,7 @@ from django.contrib.auth.models import User
 
 class Gallery(models.Model):
     user = models.ForeignKey(User, on_delete=models.CASCADE)
+    is_active = models.BooleanField(default=False, help_text="Activate/Deactivate the current gallery")
     date = models.DateTimeField(auto_now_add=True)
     
     class Meta:
@@ -16,6 +18,23 @@ class Gallery(models.Model):
     
     def __str__(self):
         return f"Gallery of {self.user.username}"
+    
+    def get_absolute_url(self):
+        return reverse('user-gallery', kwargs={})
+
+
+    
+    def images(self):
+        return Image.objects.filter(gallery=self, is_deleted=False)
+    
+    def private_images(self):
+        return Image.objects.filter(gallery=self, is_open_source=False, is_deleted=False)
+    
+    def public_images(self):
+        return Image.objects.filter(gallery=self, is_open_source=True, is_deleted=False)
+    
+    def deleted_images(self):
+        return Image.objects.filter(gallery=self, is_deleted=True)
 
 
 
@@ -26,6 +45,7 @@ class Image(models.Model):
     gallery = models.ForeignKey(Gallery, on_delete=models.CASCADE)
     image = models.ImageField(upload_to='images/%Y/%m/')
     is_open_source = models.BooleanField(default=False, help_text="Check if you want to share this picture with everyone")
+    is_deleted = models.BooleanField(default=False, help_text="Deleted (deactivate) the current image")
     date = models.DateTimeField(auto_now_add=True)
     
     class Meta:
